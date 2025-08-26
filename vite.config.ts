fd74aed692d41878e0cf87780364d34d99f6f502
EH. Eddahbi <92024331+eddahbihossine@users.noreply.github.com>
2025-08-26 18:20:58 +0100

    Remove unnecessary comment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
