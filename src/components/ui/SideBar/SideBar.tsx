d471700196b04a098caeaefe12c2d5c26e853c76
yahya mdarhri <78668874+yahya-mdarhri@users.noreply.github.com>
2026-03-28 08:41:07 +0100

    Update SideBar.tsx

--- src/components/ui/SideBar/SideBar.tsx
+++ src/components/ui/SideBar/SideBar.tsx
@@ -1,5 +1,5 @@
 import './SideBar.css'
-import { Home, Ticket, FileText, Users, User, Settings, LogOut } from 'lucide-react';
+import { Home, Ticket, FileText, Users, User, Settings, LogOut, Shield, BookOpen } from 'lucide-react';
 import {
   Sidebar,
   SidebarContent,
@@ -18,8 +18,8 @@ const mainItems = [
   { title: 'Tickets', url: '/tickets', icon: Ticket },
   { title: 'Patents', url: '/patents', icon: FileText },
   { title: 'CoInventors', url: '/coinventors', icon: Users },
-  { title: 'Charte de Propriété Intellectuelle', url: '/documents/charte-propriete-intellectuelle', icon: FileText },
-  { title: 'RÈGLEMENT INTÉRIEUR DE PROPRIÉTÉ INTELLECTUELLE', url: '/documents/reglement-interieur-propriete-intellectuelle', icon: FileText },
+  { title: 'Charte PI', url: '/documents/charte-propriete-intellectuelle', icon: Shield },
+  { title: 'Règlement PI', url: '/documents/reglement-interieur-propriete-intellectuelle', icon: BookOpen },
 ];
 
 const footerItems = [
